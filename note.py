fae44b4879c3b28d1b2cf089501b3ca5b32df0f0
MohammedAlkarbly <mohammedlearn45@gmail.com>
2021-10-05 17:51:39 +0300

    add edit an old file , date as name of file

--- note.py
+++ note.py
@@ -1,17 +1,18 @@
+from datetime import *
 p=input("enter tha path of work\n:->>\t")#ask user for a path to save foles
 while True :#the main loop
-    txt="""enter w to write a new note
+    txt="""enter c to creat a new note
 enter s to show a file
+enter e to edit a file
 enter x to exit
 ***********************************************\n"""#the program controll key
     print(txt)#the program help
     ope=input(":->>\t")#what do you wont to do
-    if ope == 'w':#write a new note
+    if ope == 'c':#creat a new note
         note=input("enter your note here:\n")
-        name=note[0:10]
+        name=str(datetime.now().date())# the name of the file is the date of day
         file=open(p+name+".txt","w+")
         file.write(note)
-        print(p+name+".txt")
         file.close()
     elif ope=='s':# show an old file
         name=input("enter file name here:\n")
@@ -21,6 +22,17 @@ enter x to exit
         
         file.close()
         print(t+"\n************************************\n")
+    elif ope=='e':# edite an old file
+        name=input("enter file name here:\n")
+        file=open(p+name,"r+")
+        file.seek(0)
+        t=file.read()
+        file.close()
+        print(t+"\n************************************\n")# print the old text 
+        file=open(p+name,"w")
+        note=input("enter the new note text")
+        file.write(note)# writ a new note text
+        file.close()
     elif ope=='x':
         break # exit
     else :
